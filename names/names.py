e3c3c8f0acbd644c18c96b81144b52554189bc40
Nicholas Pung <nckpng95@gmail.com>
2019-10-18 10:32:18 -0700

    Found a faster method for finding duplicates using dict

--- names/names.py
+++ names/names.py
@@ -13,21 +13,41 @@ f.close()
 
 # Changing this
 # Runtime: 5.519491910934448 seconds
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
-# Answer for Sprint
+# Answer for Sprint (Using BST)
 # Runtime: 0.09100198745727539 seconds
-bst = BinarySearchTree(names_1[0])
+# bst = BinarySearchTree(names_1[0])
+# duplicates = []
+# for name_1 in range(1, len(names_1)):
+#     bst.insert(names_1[name_1])
+# for name_2 in names_2:
+#     if bst.contains(name_2):
+#         duplicates.append(name_2)
+
+# Better answer for Spring
+# Runtime: 0.0084228515625 seconds
 duplicates = []
-for name_1 in range(1, len(names_1)):
-    bst.insert(names_1[name_1])
+holder = {}
+for name_1 in names_1:
+    if name_1 in holder:
+        holder[name_1] += 1
+    else:
+        holder[name_1] = 0
+
 for name_2 in names_2:
-    if bst.contains(name_2):
-        duplicates.append(name_2)
+    if name_2 in holder:
+        holder[name_2] += 1
+    else:
+        holder[name_2] = 0
+
+for key in holder:
+    if holder[key] > 0:
+        duplicates.append(key)
 
 # Answer for Stretch
 # Used python's built in verifier, runtime: 1.0983390808105469 seconds
