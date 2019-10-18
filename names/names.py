3467d9a9185969fec6385a0a958351e76f4e1b10
Nicholas Pung <nckpng95@gmail.com>
2019-10-18 10:12:57 -0700

    Did stretch, but couldn't get faster runtime

--- names/names.py
+++ names/names.py
@@ -12,13 +12,15 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 # Changing this
-# duplicates = []
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
-#
+# Runtime: 5.519491910934448 seconds
+duplicates = []
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
 
+# Answer for Sprint
+# Runtime: 0.09100198745727539 seconds
 bst = BinarySearchTree(names_1[0])
 duplicates = []
 for name_1 in range(1, len(names_1)):
@@ -26,6 +28,14 @@ for name_1 in range(1, len(names_1)):
 for name_2 in names_2:
     if bst.contains(name_2):
         duplicates.append(name_2)
+
+# Answer for Stretch
+# Used python's built in verifier, runtime: 1.0983390808105469 seconds
+# Worse than using BST, but better in terms of memory (doesn't use additional memory except to store results)
+# duplicates = []
+# for name_1 in names_1:
+#     if name_1 in names_2:
+#         duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
